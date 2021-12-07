4b7ea565b249d844fbcaf93d79fca3d4514f7388
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-12-07 19:14:33 -0300

    create configs

--- src/Modules/users/users.service.ts
+++ src/Modules/users/users.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'Database/Entities/User.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './Dtos/createuser.dto';
 import { UpdateUserDto } from './Dtos/updateUser.dto';
+
+interface IUser {
+  id: string;
+  username: string;
+  email: string;
+}
 
 @Injectable()
 export class UsersService {
@@ -20,8 +31,20 @@ export class UsersService {
     return this.usersRepository.findOne(id);
   }
 
-  create(dto: CreateUserDto) {
-    this.usersRepository.insert({ name: dto.name, email: dto.email });
+  async create(dto: CreateUserDto) {
+    const { email } = dto;
+    const emailAlreadyExists = await this.usersRepository.findOne({
+      email,
+    });
+
+    if (emailAlreadyExists) {
+      throw new UnauthorizedException('Email already exists');
+    }
+
+    await this.usersRepository.insert({
+      name: dto.name,
+      email: dto.email,
+    });
   }
 
   update(id: string, dto: UpdateUserDto) {
